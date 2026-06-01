55032966178c2f739d0d9fe334b4858a1c02b853
Aravind Srinivas <arvnd.man.007@gmail.com>
2026-06-01 15:13:38 +0530

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        std::unordered_set<int> hashset;
+        std::unordered_set<int> startIndex;
+
+        hashset.insert(nums.begin(), nums.end());
+        for (int i : nums){
+            if (hashset.count(i+1)){
+                startIndex.insert(i);
+            }
+        }
+        std::vector<int> counts;
+        for (int i : startIndex){
+            int counter = 1;
+            for (int j = 1; j < nums.size(); j++){
+                if (hashset.count(i+j)) ++counter;
+                else break;
+            }
+            counts.push_back(counter);
+        }
+        if (startIndex.size() > 0)
+            return static_cast<int>(*std::max_element(counts.begin(), counts.end()));
+        else return 1;
+    }
+};
