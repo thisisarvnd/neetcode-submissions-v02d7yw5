2a0566eaa713704b16dcc87e8df73ec745d45522
Aravind Srinivas <arvnd.man.007@gmail.com>
2026-06-09 20:03:30 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // brute forcing just to check TLE and to get a flow
+        vector<vector<int>> solution;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        vector<int> pairs = {nums[i], nums[j], nums[k]};
+                        sort(pairs.begin(), pairs.end());
+                        
+                        // Replicating "if pairs in solution:"
+                        if (find(solution.begin(), solution.end(), pairs) != solution.end()) {
+                            continue;
+                        } else {
+                            solution.push_back(pairs);
+                        }
+                    }
+                    
+                }
+            }
+        }
+        return solution;
+    }
+};
