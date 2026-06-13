be46a85a29b9b8198511d80f5bf80191d1f8fce2
Aravind Srinivas <arvnd.man.007@gmail.com>
2026-06-13 19:54:17 +0530

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        startptr = 0
+        endptr = len(s) - 1
+        while startptr <= endptr:
+            print("loop1",startptr,endptr)
+            if s[startptr] == s[endptr]:
+                startptr += 1
+                endptr -= 1
+                continue
+            else:
+                temp_s = startptr
+                temp_e = endptr
+                startptr += 1
+                while startptr <= endptr:
+                    print("loop2",startptr,endptr)
+                    if s[startptr] == s[endptr]:
+                        startptr += 1
+                        endptr -= 1
+                        continue
+                    else:
+                        break
+                    return True
+                
+                startptr = temp_s
+                endptr = temp_e -1
+                while startptr <= endptr:
+                    print("loop3",startptr,endptr)
+                    if s[startptr] == s[endptr]:
+                        startptr += 1
+                        endptr -= 1
+                        continue
+                    else:
+                        return False
+        return True
